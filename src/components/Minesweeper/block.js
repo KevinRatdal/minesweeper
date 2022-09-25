b175f3631be167339d38286dd2815943970d6912
Kevin Ratdal <kevin1999@live.no>
2022-09-25 22:11:50 +0200

    neighbor logic implemented kinda

--- src/components/Minesweeper/block.js
+++ src/components/Minesweeper/block.js
@@ -8,6 +8,7 @@ const Block = (props) => {
   const [clicked, setClicked] = useState(false)
   const [flagged, setFlagged] = useState(false)
   //const [bomb, setBomb] = useState(false)
+  const num = blockElement.neighborBombs
 
   const handleClick=(e) => {
     console.log(e)
@@ -21,29 +22,58 @@ const Block = (props) => {
   }
 
   return (
-    <div 
+    <div
       onClick={handleClick}
-      style={{ 
-        width: '100%', 
-        height: '100%', display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        backgroundColor: clicked ? 'lightgrey' : 'darkgrey', 
-        border: clicked ? '2px solid darkgray' : '2px solid black', 
-        overflow: 'hidden'
-      }}>
-      {clicked ? 
-        blockElement.isBomb ? 
-          <Mine width="40px"/> :
-          <span>{blockElement?.value}</span>
-      :
-      flagged ? 
-          <Flag width="40px" />
-      :
-        null
-      }
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: clicked ? "lightgrey" : "darkgrey",
+        border: clicked ? "2px solid darkgray" : "2px solid black",
+        overflow: "hidden",
+      }}
+    >
+      {/* <p>
+        ({blockElement.x},{blockElement.y}, {blockElement.neighborBombs})
+      </p> */}
+      {clicked ? (
+        blockElement.isBomb ? (
+          <Mine width="40px" />
+        ) : (
+          <Number number={num} />
+        )
+      ) : flagged ? (
+        <Flag width="40px" />
+      ) : null}
     </div>
-  )
+  );
 }
 
 export default Block
+
+
+const Number = ({number}) => {
+  const numToCol = {
+    0: "FFFFFF",
+    1: "#0000FF",
+    2: "#00FF00",
+    3: "#FF0000",
+    4: "#00008B",
+    5: "#964B00",
+    6: "#00FFFF",
+    7: "#000000",
+    8: "#808080",
+  };
+
+  return (
+    <span 
+      style={{
+        color: numToCol[number]
+      }}
+    >
+    {number !== 0 && number}
+    </span>
+  )
+}
--- src/components/Minesweeper/index.js
+++ src/components/Minesweeper/index.js
@@ -1,61 +1,126 @@
-import React, { useEffect, useState } from 'react'
-import Block from './block'
+import React, { useEffect, useState } from "react";
+import Block from "./block";
+
+const height = 6;
+const width = 5;
+const numBombs = 5;
 
 const Minesweeper = () => {
-
-  const [flagOnClick, setFlagOnClick] = useState(false)
-  const [fields, setFields] = useState(null)
+  const [flagOnClick, setFlagOnClick] = useState(false);
+  const [fields, setFields] = useState(null);
 
   useEffect(() => {
-    setFields(generateFields(5,5,4))
-  },[])
+    setFields(generateFields(height, width, numBombs));
+  }, []);
 
   const handleFlagButton = () => {
-    setFlagOnClick(prev => !prev) 
-  }
+    setFlagOnClick((prev) => !prev);
+  };
 
-
-  
-
-  const blocks = fields && fields.map((block, i) => {
-    return <Block key={i} blockElement={block} flagOnClick={flagOnClick}/>
-  } )
+  const blocks =
+    fields &&
+    fields.map((block, i) => {
+      return <Block key={i} blockElement={block} flagOnClick={flagOnClick} />;
+    });
 
   return (
-    <div style={{ padding: "2px", background: "lightgray", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '1em', paddingTop: '1em'}}>
+    <div
+      style={{
+        padding: "2px",
+        background: "lightgray",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "1em",
+          paddingTop: "1em",
+        }}
+      >
         <p>squares left:</p>
-        <button onClick={handleFlagButton}>flag {flagOnClick ? "y" : "n"}</button>
+        <button onClick={handleFlagButton}>
+          flag {flagOnClick ? "y" : "n"}
+        </button>
         <p>bombs: 10</p>
       </div>
-      <div style={gridStyle(5,5)}>
-        {blocks}
-      </div>
+      <div style={gridStyle(height, width)}>{blocks}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Minesweeper
+export default Minesweeper;
 
-const gridStyle = (width, height) => ({
-  display: 'grid',
+const gridStyle = (height, width) => ({
+  display: "grid",
   gridTemplateRows: `repeat(${height}, 50px)`,
   gridTemplateColumns: `repeat(${width}, 50px)`,
-  gap: '2px',
-  padding: '1em'
-})
+  gap: "2px",
+  padding: "1em",
+});
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
 const generateFields = (height, width, numBombs) => {
-  let fields = []
-  let bombCount = 0
+  let fields = [];
+  let bombs = [
+    ...[...new Array(numBombs)].map(() => true),
+    ...[...new Array(height * width - numBombs)].map(() => false),
+  ];
+  shuffleArray(bombs);
+  console.log(bombs);
+
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      let GonnaBeBomb = bombCount < numBombs ? (Math.floor(Math.random() * 10) < 4) : false
-      fields.push({ x: x, y: y, isFlag: false, isBomb: GonnaBeBomb,  })
-      if (GonnaBeBomb) {
-        bombCount = bombCount + 1
-      }
+      let totalIndex = y * width + x;
+      console.log(y, x, totalIndex);
+      fields.push({
+        x: x,
+        y: y,
+        neighborBombs: neighborBombs(bombs, totalIndex, height, width),
+        isFlag: false,
+        isBomb: bombs[totalIndex],
+      });
     }
   }
-  return fields
+  console.log(fields);
+  return fields;
+};
+
+
+const neighborBombs = (bombs, totalIndex, height, width ) => {
+  let numNeighborBombs = 0
+  const rightEdge = (totalIndex % width) === width -1
+  const leftEdge = (totalIndex % width) === 0
+  const topEdge = totalIndex < width
+  const bottomEdge = totalIndex >= ((height-1)*width)
+  const indexesToCheck = [
+    !topEdge && !leftEdge && totalIndex - width - 1,
+    !topEdge && totalIndex - width,
+    !topEdge && !rightEdge && totalIndex - width + 1,
+    !leftEdge && totalIndex - 1,
+    !rightEdge && totalIndex + 1,
+    !bottomEdge && !leftEdge && totalIndex + width -1,
+    !bottomEdge && totalIndex + width,
+    !bottomEdge && !rightEdge && totalIndex + width +1,
+  ];
+  console.log(totalIndex, indexesToCheck)
+
+  indexesToCheck.forEach(check => {
+    if (typeof check === "number") {
+      if (bombs[check]) {
+        numNeighborBombs += 1
+      }
+    }
+  })
+  return numNeighborBombs
 }
